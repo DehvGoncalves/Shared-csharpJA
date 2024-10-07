bebbf56e3dc79c5f7b4ee5238d42999985484af6
Dehv <71988020+DehvGoncalves@users.noreply.github.com>
2024-10-07 15:51:15 -0300

    Add files via upload

---
+++ SharedCsharpJa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SharedCsharpJa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
